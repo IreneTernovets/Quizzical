7194db128c3d8c5ade96362380ae7402e15d186d
Irene <terna.legend@gmail.com>
2024-01-27 18:03:48 +0100

    added fatching data

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ function App() {
     
     <Background>
       <main>
-        <h1>Quizzical</h1>
-        <p className='quiz-desc'>In this quiz game, players respond to multiple-choice questions
-          by choosing the correct option from several answers. Points are awarded for each correct response,
-          aiming to test knowledge and quick thinking without a competitive winner.</p>
-        <Link to="/quiz">Start quiz</Link>
+        <h1>👾Quizzical</h1>
+        <p className='quiz-desc'>This quiz game involves answering multiple-choice questions correctly to earn points.
+          It focuses on testing knowledge and quick decision-making in a non-competitive format.</p>
+        <Link to="/quiz" className="start-button">Start quiz</Link>
       </main>
     </Background>
     
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Background from '../components/Background'
+import QuestionElement from '../components/QuestionElement'
 
 const Quiz = () => {
+  const [questions, setQuestions] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple')
+      .then(res => res.json())
+      .then(data => setQuestions(data.results))
+  }, [])
+
+
+ 
+
   return (
-    <div>Quiz</div>
+    <Background>
+      {questions && questions.map(item => (
+        <QuestionElement />
+      ))}
+    </Background>
   )
 }
 
