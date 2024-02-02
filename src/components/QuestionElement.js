1933a6a0d6acb54e4e62c673316d881ba8cb4c22
Irene <terna.legend@gmail.com>
2024-02-02 19:02:11 +0100

    added shuffling to the answers array

--- src/components/QuestionElement.js
+++ src/components/QuestionElement.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const QuestionElement = (props) => {
+  console.log(props.shuffledAnswers)
   return (
-    <div>{props.question}</div>
+    <>
+      <div className='question'>{props.question}</div>
+      <div>{props.shuffledAnswers}</div>
+    </>
   )
 }
 
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Background from '../components/Background'
 import QuestionElement from '../components/QuestionElement'
+import { decode } from 'html-entities';
+import { nanoid } from 'nanoid'
 
 const Quiz = () => {
   const [questions, setQuestions] = React.useState([])
@@ -10,19 +12,36 @@ const Quiz = () => {
       const response = await fetch('https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple');
       const data = await response.json();
       setQuestions(data.results);
+      console.log(data.results)
     };
     if (questions.length === 0) {
       fetchData();
     }
   }, []);
 
+  function shuffleArray(array) {
+    let shuffledArray = array.slice();
 
-  const QuestionElements = questions.map(question => (<QuestionElement />))
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+
+    return shuffledArray;
+  }
+
+  const QuestionElements = questions.map(question => (<QuestionElement
+    key={nanoid()}
+    question={decode(question.question)}
+    shuffledAnswers={shuffleArray([...question.incorrect_answers, question.correct_answer])}
+  />))
 
 
   return (
     <Background>
-      {QuestionElements}
+      <div className='quiz-body'>
+        {QuestionElements}
+      </div>
     </Background>
   );
 };
